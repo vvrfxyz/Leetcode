b426ac74a8bc90dfd9d9d3cc5c2207448ed7fd8d
温瑞峰 <vvrfxyz@gmail.com>
2022-08-04 22:21:04 +0800

    maxProfit_无数组方法

--- src/maxProfit.java
+++ src/maxProfit.java
@@ -13,4 +13,24 @@ public class maxProfit {
         }
         return dp[length-1][0];
     }
+
+    /**
+     * 因当天的利润只与前一天持有股票或不持有股票有关，所以用两个变量存储值，省去二维数组。
+     * 最后一天手里没有股票才会利润最大化，所以返回值为不持有股票的变量。
+     * @param prices
+     * @return
+     */
+    int maxProfit_2(int[] prices) {
+        if (prices==null || prices.length<2) {
+            return 0;
+        }
+        int length=prices.length;
+        int hold=-prices[0];
+        int noHold=0;
+        for (int i=1;i<length;i++){
+            hold=Math.max(hold,noHold-prices[i]);
+            noHold=Math.max(noHold,hold+prices[i]);
+        }
+        return noHold;
+    }
 }
