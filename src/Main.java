f826b49a40cfec9e059bf3bf4f063d5da4ae05be
温瑞峰 <wenruifeng@sinosoft.com.cn>
2022-08-10 18:02:04 +0800

    旋转

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate date=LocalDate.parse("2022-02-03");
+    }
+}
---
+++ src/containsDuplicate.java
@@ -0,0 +1,19 @@
+public class containsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums.length==1 || nums==null){
+            return false;
+        }
+        for (int i=0;i< nums.length;i++){
+            int k=nums[i];
+            for (int g=i+1;g< nums.length;g++){
+                if (nums[g]==k)return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        return false;
+    }
+}
